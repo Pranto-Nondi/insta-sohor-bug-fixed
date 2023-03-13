8f32eae2bea8206a7d8e1950739ae84783714d35
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-03-13 13:31:21 +0600

    eror handeling

--- js/app.js
+++ js/app.js
@@ -16,7 +16,9 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  likedPostsId.plus(id);
+  console.log(id);
+  console.log(likedPostsId)
+  likedPostsId.push(id);
   showPosts(posts);
 };
 
